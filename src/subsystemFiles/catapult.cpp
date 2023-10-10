d8a5ad7f9b2abb7fb550cbfe8f9040b7fb932115
9909Y-Robotics <24battistar@s.mpsct.org>
2023-10-10 11:34:06 -0400

    Scale cata speed to 75%

--- src/subsystemFiles/catapult.cpp
+++ src/subsystemFiles/catapult.cpp
@@ -2,10 +2,10 @@
 
 void setCata() {
     if (controller.get_digital(E_CONTROLLER_DIGITAL_A)) {
-        catapult = (127.0 / 2);
+        catapult = (127.0 * 0.75);
     }
     else if (controller.get_digital(E_CONTROLLER_DIGITAL_B)) {
-        catapult = (-127.0 / 2);
+        catapult = (-127.0 * 0.75);
     }
     else {
         catapult = 0;
