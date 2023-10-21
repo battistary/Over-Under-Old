e0f8a5a171e57d279119582fc37548beae1091a9
9909Y-Robotics <24battistar@s.mpsct.org>
2023-10-21 08:16:50 -0400

    Reverse robot orientation for driver control

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -15,8 +15,8 @@
  */
 void opcontrol() {
     while ( true ) {
-        double left = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) + controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
-        double right = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) - controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
+        double left = (-1) * (controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) + controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X));
+        double right = (-1) * (controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y) - controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X));
         driveLeft.move(left);
         driveRight.move(right);
         
